e362d759138c208846919843bdadf565d2528f6b
nnmax1 <nngai6019@gmail.com>
2023-05-24 09:46:29 -0400

    initial

---
+++ goatorcat.js
@@ -0,0 +1,57 @@
+var card1= ["goat", "goat"]
+var card2 = ["goat", "cat"]
+var card3 = ["cat", "cat"]
+var card4 = ["cat", "goat"]
+var cardArr = [card1, card2, card3, card4]
+catimg='https://i.imgur.com/AMTogc2.png'
+goatimg='https://i.imgur.com/DNNNCrn.png'
+var numCats = 0
+var numGoats = 0
+var rand = 0
+
+function show() {
+    var side2 = document.getElementById("side2")
+    side2.style.opacity= "0"
+    var side1 = document.getElementById("side1")
+    var side2 = document.getElementById("side2")
+
+    // get a card from the array
+    rand= Math.floor((Math.random() * 4));
+    // get the side of the card (goat or cat)
+    if(cardArr[rand][0] === 'cat'){
+        side1.setAttribute("src",catimg)
+    }
+    if(cardArr[rand][1] === 'cat'){
+        side2.setAttribute("src",catimg)
+    }
+    if(cardArr[rand][0] === 'goat'){
+        side1.setAttribute("src", goatimg)
+    }
+    if(cardArr[rand][1] === 'goat'){
+        side2.setAttribute("src", goatimg)
+    }
+}
+
+function reveal() {
+    var catCount = document.getElementById("catCount")
+    var goatCount = document.getElementById("goatCount")
+    var side2 = document.getElementById("side2")
+    side2.style.opacity= "100%"
+    if(cardArr[rand][1] === "cat") {
+        numCats = numCats+1
+        catCount.innerText = numCats
+    }
+     if(cardArr[rand][1] === "goat") {    
+        numGoats = numGoats + 1;
+        goatCount.innerText = numGoats
+    }
+}
+
+function resetCount (){
+    var catCount = document.getElementById("catCount")
+    var goatCount = document.getElementById("goatCount")
+    numCats = 0
+    numGoats = 0
+    catCount.innerText = numCats
+    goatCount.innerText = numGoats
+}
